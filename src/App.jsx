32ee321b8072e029a3f556dd801e3cc43242ad26
meenatchi2007 <marameena66@gmail.com>
2026-05-12 15:18:20 +0530

    Change header text from 'AWS' to 'Meenatchi M'

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>AWS</h1>
+          <h1>Meenatchi M</h1>
           <p>
           <code>CI CD</code>
           </p>
